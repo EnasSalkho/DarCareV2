f4b7c7031244717e38dca163a213e7a776e6a419
Aya Hamoudeh <ayahamoudeh29@gmail.com>
2026-09-06 19:32:46 +0300

    feat: setup broadcasting and pusher

--- DarCareV1-main/routes/api.php
+++ DarCareV1-main/routes/api.php
@@ -2,6 +2,11 @@
 // routes/api.php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Broadcast; // أضيفي هذا الكلاس
+
+// تعريف راوت البث ليعمل بنظام API مع Sanctum
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 
 Route::prefix('v1')->group(function () {
     require base_path('app/Modules/Auth/routes/api.php');
